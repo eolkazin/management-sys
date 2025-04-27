fb04397154ec877166a9bfd55f48672222194e20
Lucas Guerra <125227136+eolkazin@users.noreply.github.com>
2025-04-27 16:48:10 -0300

    consertanto bugs db

--- projeto_x/app_sys/models.py
+++ projeto_x/app_sys/models.py
@@ -1,16 +1,7 @@
 from django.db import models
 
-### Modelo de usuário com campos de username, password e tipo ###
+
+### Modelo de usuário com campos de username e password ###
 class Usuario(models.Model):
     username = models.CharField(max_length=150, unique=True)  # Nome de usuário único
     password = models.CharField(max_length=128)  # Senha do usuário
-    tipo = models.CharField(
-        max_length=50,
-        choices=[('financeiro', 'Financeiro'), 
-                 ('estoque', 'Estoque'),
-                 ('dev', 'Desenvolvimento')],
-        default='estoque'  # Define 'estoque' como valor padrão
-    )
-
-    def __str__(self):
-        return self.username
